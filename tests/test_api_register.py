ab387af025c2f6d98c4c16e9c3ec9be547d85ace
g3n35i5 <janfschmidt@me.com>
2018-08-16 11:28:25 +0200

    Unittest: API: Register user with existing email/username.

--- tests/test_api_register.py
+++ tests/test_api_register.py
@@ -5,7 +5,7 @@ import shopdb.models as models
 import shopdb.exceptions as exc
 from sqlalchemy.exc import *
 from time import sleep
-from base import u_emails, u_passwords
+from base import u_emails, u_passwords, u_firstnames, u_lastnames, u_usernames
 from base_api import BaseAPITestCase
 from flask import json
 import jwt
@@ -26,3 +26,45 @@ class RegisterAPITestCase(BaseAPITestCase):
         res = self.post(url='/register', data=data)
         self.assertEqual(res.status_code, 200)
         self.assertIn(b'Created user.', res.data)
+
+        user = User.query.filter_by(id=5).first()
+        self.assertTrue(user)
+        self.assertEqual(user.firstname, 'John')
+        self.assertEqual(user.lastname, 'Doe')
+        self.assertEqual(user.username, 'johnny')
+        self.assertEqual(user.email, 'john.doe@test.com')
+        self.assertFalse(user.is_verified)
+
+    def test_register_existing_email(self):
+        '''This test should ensure that the correct exception gets returned
+           on creating a user with an email address already taken.'''
+        data = {
+            'firstname': 'John',
+            'lastname': 'Doe',
+            'username': 'johnny',
+            'email': u_emails[0],
+            'password': 'supersecret',
+            'repeat_password': 'supersecret'
+        }
+        res = self.post(url='/register', data=data)
+        self.assertException(res, EmailAddressAlreadyTaken)
+
+        user = User.query.filter_by(id=5).first()
+        self.assertFalse(user)
+
+    def test_register_existing_username(self):
+        '''This test should ensure that the correct exception gets returned
+           on creating a user with an username already taken.'''
+        data = {
+            'firstname': 'John',
+            'lastname': 'Doe',
+            'username': u_usernames[0],
+            'email': 'john.doe@test.com',
+            'password': 'supersecret',
+            'repeat_password': 'supersecret'
+        }
+        res = self.post(url='/register', data=data)
+        self.assertException(res, UsernameAlreadyTaken)
+
+        user = User.query.filter_by(id=5).first()
+        self.assertFalse(user)
