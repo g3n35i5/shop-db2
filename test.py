c178de17823a6694d99e3dbaf4de6af0ae142795
Tim737 <tim.rambousky@online.de>
2018-08-17 15:50:32 +0200

    Unittest: chose specific files to test from list

--- test.py
+++ test.py
@@ -2,27 +2,52 @@
 
 import argparse
 import sys
+import os
 import unittest
 
-###Usage:   -to run all test "./test.py"
-###         -to run specific files "./test.py --file [files with path]"
-###Example: "./test.py --file tests/base.py tests/base_api.py"
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="Application to run unittest, default runs all")
-    parser.add_argument('--file', nargs='+', help="Single files to test")
-    args = parser.parse_args()
-    if args.file:
-        print(args.file)
-        list_files = args.file
-        test = []
-        for i in range(0, len(list_files)):
-            list_files[i] = list_files[i].strip("tests/")
-            test += unittest.TestLoader().discover("tests", pattern=list_files[i])
-        testcombo = unittest.TestSuite(test)
-        unittest.TextTestRunner(verbosity=2).run(testcombo)
 
-    else:
+    list_files = os.listdir('tests')
+    list_tests = []
+
+    for file in list_files:
+        try:
+            file.index('test')
+        except ValueError:
+            continue
+        else:
+            list_tests.append(file)
+
+
+    print('Please select the tests you want to run [Default=0] \n'
+            '0: All tests')
+    for i in range(0, len(list_tests)):
+        print('{}: {}'.format(i+1, list_tests[i]))
+    answ = input("Testnumbers: ")
+
+
+    if answ == "" or answ == "0":
         tests = unittest.TestLoader().discover('tests')
         unittest.TextTestRunner(verbosity=2).run(tests)
+    else:
+        testnumbers = answ.split(" ")
+
+        for i in range(0, len(testnumbers)):
+            try:
+                testnumbers[i] = int(testnumbers[i])-1
+            except ValueError:
+                print("Invalid input")
+                sys.exit()
+        if  -1 in testnumbers:
+            print("Invalid input")
+            sys.exit()
+
+        test = []
+        for i in testnumbers:
+            if i > (len(list_tests)-1):
+                print("Invalid input")
+                sys.exit()
+            test.append(unittest.TestLoader().discover("tests", pattern=list_tests[i]))
+        testcombo = unittest.TestSuite(test)
+        unittest.TextTestRunner(verbosity=2).run(testcombo)
