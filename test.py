f559cf73ce6139a4f67e7398cd15b93d69a42540
g3n35i5 <janfschmidt@me.com>
2018-08-27 17:32:40 +0200

    Test: lintering and changing 'a' to 'all'.

--- test.py
+++ test.py
@@ -19,19 +19,17 @@ if __name__ == '__main__':
         else:
             list_tests.append(file)
 
-
-    print('Please select the tests you want to run [Default=a] \n'
-            'a: All tests')
-    for i, test in enumerate(list_tests) :
+    print('Please select the tests you want to run [Default=all] \n'
+          'all: All tests')
+    for i, test in enumerate(list_tests):
         print('{}: {}'.format(i, test))
-    answ = input("Testnumbers: ")
+    answ = input('Testnumbers: ')
 
-
-    if answ in ['', 'a']:
+    if answ in ['', 'all', 'a']:
         tests = unittest.TestLoader().discover('tests')
         unittest.TextTestRunner(verbosity=2).run(tests)
     else:
-        list_testnumbers = answ.split(" ")
+        list_testnumbers = answ.split(' ')
         test = []
 
         for i, testnumber in enumerate(list_testnumbers):
@@ -39,14 +37,13 @@ if __name__ == '__main__':
                 testnumber = int(testnumber)
 
             except ValueError:
-                print("Invalid input")
-                sys.exit()
+                sys.exit('Invalid input')
 
             if testnumber not in range(0, len(list_tests)):
-                print("Invalid input")
-                sys.exit()
+                sys.exit('Invalid input')
 
-            test.append(unittest.TestLoader().discover("tests", pattern=list_tests[testnumber]))
+            test.append((unittest.TestLoader()
+                         .discover('tests', pattern=list_tests[testnumber])))
 
         testcombo = unittest.TestSuite(test)
         unittest.TextTestRunner(verbosity=2).run(testcombo)
