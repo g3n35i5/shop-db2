dad76c2d81e4d221e265b5b274c74aee44c9ee8d
g3n35i5 <janfschmidt@me.com>
2018-11-09 10:31:01 +0100

    API: Added missing import

--- shopdb/api.py
+++ shopdb/api.py
@@ -2,7 +2,7 @@
 
 from shopdb.models import *
 import shopdb.exceptions as exc
-from flask import (Flask, request, g, make_response, jsonify)
+from flask import (Flask, request, g, make_response, jsonify, send_from_directory)
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from werkzeug.exceptions import RequestEntityTooLarge, NotFound
