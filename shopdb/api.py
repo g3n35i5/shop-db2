afb3316d6111259dedcdd0423b951d1ad752b801
g3n35i5 <janfschmidt@me.com>
2018-11-13 07:49:07 +0100

    API: Lintering docstring for GET /products/<int:id>

--- shopdb/api.py
+++ shopdb/api.py
@@ -1014,18 +1014,18 @@ def create_product(admin):
 @adminOptional
 def get_product(admin, id):
     """
-        Returns the product with the requested id.
+    Returns the product with the requested id.
 
-        :param admin:               Is the administrator user, determined by
-                                    @adminOptional.
-        :param id:                  Is the product id.
+    :param admin:               Is the administrator user, determined by
+                                @adminOptional.
+    :param id:                  Is the product id.
 
-        :return:                    The requested product as JSON object.
+    :return:                    The requested product as JSON object.
 
-        :raises ProductNotFound:    If the product with this ID does not exist.
-        :raises UnauthorizedAccess: If the product is inactive and the request
-                                    does not come from an administrator.
-        """
+    :raises ProductNotFound:    If the product with this ID does not exist.
+    :raises UnauthorizedAccess: If the product is inactive and the request
+                                does not come from an administrator.
+    """
     product = Product.query.filter(Product.id == id).first()
     if not product:
         raise exc.ProductNotFound()
