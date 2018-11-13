1c222d6c22cb6a4ab993b9cc64dadf71c3d394d6
g3n35i5 <janfschmidt@me.com>
2018-11-13 10:16:38 +0100

    Tests: More unittests for upload

--- tests/test_api_upload.py
+++ tests/test_api_upload.py
@@ -24,9 +24,24 @@ class UploadAPITestCase(BaseAPITestCase):
         self.assertException(res, exc.NoFileIncluded)
         self.assertEqual(len(Upload.query.all()), 0)
 
+    def test_upload_empty_filename(self):
+        """A request with an empty filename should raise an error."""
+        image = {'value': base64.b64encode(b'abc').decode()}
+        res = self.post(url='/upload', data=image, role='admin')
+        self.assertException(res, exc.InvalidFilename)
+        self.assertEqual(len(Upload.query.all()), 0)
+
+    def test_upload_no_value_field(self):
+        """A request without a value should raise an error."""
+        image = {'filename': 'test.png'}
+        res = self.post(url='/upload', data=image, role='admin')
+        self.assertException(res, exc.NoFileIncluded)
+        self.assertEqual(len(Upload.query.all()), 0)
+
     def test_upload_invalid_extension(self):
         """A request with an invalid file extension should raise an error."""
-        image = {'filename': 'test.abc', 'value': base64.b64encode(b'abc').decode()}
+        image = {'filename': 'test.abc',
+                 'value': base64.b64encode(b'abc').decode()}
         res = self.post(url='/upload', data=image, role='admin')
         self.assertException(res, exc.InvalidFileType)
         self.assertEqual(len(Upload.query.all()), 0)
@@ -34,7 +49,8 @@ class UploadAPITestCase(BaseAPITestCase):
     def test_upload_file_too_large(self):
         """A request with an file which is too large should raise an error."""
         bytes = b'1' * 6 * 1024 * 1024
-        image = {'filename': 'test.png', 'value': base64.b64encode(bytes).decode()}
+        image = {'filename': 'test.png',
+                 'value': base64.b64encode(bytes).decode()}
         res = self.post(url='/upload', data=image, role='admin')
         self.assertException(res, exc.FileTooLarge)
         self.assertEqual(len(Upload.query.all()), 0)
@@ -51,17 +67,41 @@ class UploadAPITestCase(BaseAPITestCase):
         filepath = app.config['UPLOAD_FOLDER'] + 'broken_image.png'
         with open(filepath, 'rb') as test:
             bytes = test.read()
-        image = {'filename': 'broken.png', 'value': base64.b64encode(bytes).decode()}
+        image = {'filename': 'broken.png',
+                 'value': base64.b64encode(bytes).decode()}
         res = self.post(url='/upload', data=image, role='admin')
         self.assertException(res, exc.BrokenImage)
         self.assertEqual(len(Upload.query.all()), 0)
+
+    def test_invalid_filetype_hidden_behind_valid_filename(self):
+        """A request an invalid file type should raise an error."""
+        filepath = app.config['UPLOAD_FOLDER'] + 'valid_image.jpg'
+        with open(filepath, 'rb') as test:
+            bytes = test.read()
+        image = {'filename': 'hidden_invalid_filetype.png',
+                 'value': base64.b64encode(bytes).decode()}
+        res = self.post(url='/upload', data=image, role='admin')
+        self.assertException(res, exc.InvalidFileType)
+        self.assertEqual(len(Upload.query.all()), 0)
+
+    def test_upload_non_quadratic_image(self):
+        filepath = app.config['UPLOAD_FOLDER'] + 'non_quadratic.png'
+        with open(filepath, 'rb') as test:
+            bytes = test.read()
+        image = {'filename': 'non_quadratic.png',
+                 'value': base64.b64encode(bytes).decode()}
+        res = self.post(url='/upload', data=image, role='admin')
+        self.assertException(res, exc.ImageMustBeQuadratic)
+        self.assertEqual(len(Upload.query.all()), 0)
+
 
     def test_upload_valid_image(self):
         """A request with valid images should work."""
         filepath = app.config['UPLOAD_FOLDER'] + 'valid_image.png'
         with open(filepath, 'rb') as test:
             bytes = test.read()
-        image = {'filename': 'valid.png', 'value': base64.b64encode(bytes).decode()}
+        image = {'filename': 'valid.png',
+                 'value': base64.b64encode(bytes).decode()}
         res = self.post(url='/upload', data=image, role='admin')
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.data)
