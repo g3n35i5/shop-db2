300a29022deb9e4163ea0c0afcfc8a9d655b6ac8
g3n35i5 <janfschmidt@me.com>
2018-11-14 08:10:33 +0100

    Tests: Listing users adding list for registered user
    Check the listing of all users by a registered user without
    administrator rights

--- tests/test_api_list_users.py
+++ tests/test_api_list_users.py
@@ -1,29 +1,21 @@
-from shopdb.api import *
-import shopdb.models as models
-import shopdb.exceptions as exc
-from sqlalchemy.exc import *
-from time import sleep
-from base import u_emails, u_passwords, u_firstnames, u_lastnames, u_usernames
 from base_api import BaseAPITestCase
 from flask import json
-import jwt
-from copy import copy
-import pdb
 
 
 class ListUsersAPITestCase(BaseAPITestCase):
-    def test_list_users_without_token(self):
-        """Get a list of all users as user"""
-        res = self.get(url='/users')
-        self.assertEqual(res.status_code, 200)
-        data = json.loads(res.data)
-        assert 'users' in data
-        users = data['users']
-        self.assertEqual(len(users), 3)
-        for user in users:
-            self.assertEqual(len(user), 4)
-            for item in ['id', 'firstname', 'lastname', 'username']:
-                assert item in user
+    def test_list_users_as_user_and_external(self):
+        """Get a list of all users as user and as external."""
+        for role in ['user', None]:
+            res = self.get(url='/users', role=role)
+            self.assertEqual(res.status_code, 200)
+            data = json.loads(res.data)
+            assert 'users' in data
+            users = data['users']
+            self.assertEqual(len(users), 3)
+            for user in users:
+                self.assertEqual(len(user), 4)
+                for item in ['id', 'firstname', 'lastname', 'username']:
+                    assert item in user
 
     def test_list_users_with_token(self):
         """Get a list of all users as admin. It should contain more information
