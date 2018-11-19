7bcb968960a9fcebefce19b36a95af7865ceef37
Tim Rambousky <Tim.Rambousky@online.de>
2018-11-19 21:28:48 +0100

    API: First User can be added through route /initial_setup

--- configuration.example.py
+++ configuration.example.py
@@ -15,6 +15,7 @@ class BaseConfig(object):
     MAX_CONTENT_LENGTH = 4 * 1024 * 1024
     VALID_EXTENSIONS = ['png']
     MINIMUM_PASSWORD_LENGTH = 6
+    INIT_TOKEN = 'INIT'
 
 
 class ProductiveConfig(BaseConfig):
