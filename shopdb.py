0868b05610cb69d4941cc25754b5f92ab6484297
g3n35i5 <janfschmidt@me.com>
2018-11-21 08:39:23 +0100

    shopdb.py: Adjustments so that shop-db can be started in productive mode.
    Closes #8

--- shopdb.py
+++ shopdb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 
 try:
     import configuration as config
@@ -14,14 +15,24 @@ from shopdb.api import *
 from dev import insert_dev_data
 
 parser = argparse.ArgumentParser(description='Starting script shop.db')
-parser.add_argument('--mode', choices=['development'])
+parser.add_argument('--mode', choices=['development', 'productive'],
+                    default='productive')
 args = parser.parse_args()
 
 if args.mode == 'development':
+    print('Starting shop-db in developing mode')
     set_app(config.DevelopmentConfig)
     app.app_context().push()
     db.create_all()
     insert_dev_data(db)
+
+elif args.mode == 'productive':
+    if not os.path.isfile(config.ProductiveConfig.DATABASE_PATH):
+        sys.exit('No database found. Please read the documentation and use '
+                 'the setupdb.py script to initialize shop-db.')
+    print('Starting shop-db in productive mode')
+    set_app(config.ProductiveConfig)
+
 else:
     parser.print_help()
     sys.exit(f'{args.mode}: invalid operating mode')
