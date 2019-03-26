9ecce7b35e4a73ed653a9b432ef7d1db9c77d445
g3n35i5 <janfschmidt@me.com>
2019-03-26 10:23:34 +0100

    Manager, Migration: Introducing the Flask-Migration manager (alembic).
    This tool automatically generates a migration script which creates a
    traceable history of changes to the database schema.

---
+++ Manager.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from shopdb.api import app, set_app, db
+import configuration as config
+
+set_app(config.ProductiveConfig)
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
