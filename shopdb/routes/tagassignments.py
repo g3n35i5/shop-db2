faaf24ddbd910c9d799658f0a8b20c8c05049d40
g3n35i5 <janfschmidt@me.com>
2019-09-15 15:52:57 +0200

    API: Moving all tagassignment related routes to a separate file

---
+++ shopdb/routes/tagassignments.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'g3n35i5'
+
+from sqlalchemy.exc import IntegrityError
+from flask import jsonify
+import shopdb.exceptions as exc
+from shopdb.api import (app, adminRequired, db, check_fields_and_types, json_body)
+from shopdb.models import Tag, Product
+
+
+@app.route('/tagassignment/<command>', methods=['POST'])
+@adminRequired
+def change_product_tag_assignment(admin, command):
+    """
+    Under this route, a tag can be added to a product or removed.
+
+    :param admin:              Is the administrator user, determined by
+                               @adminRequired.
+
+    :return:                   A message that the assignment has been added or
+                               removed.
+
+    :raises ForbiddenField:    If a forbidden field is in the request data.
+    :raises UnknownField:      If an unknown parameter exists in the request
+                               data.
+    :raises InvalidType:       If one or more parameters have an invalid type.
+    :raises EntryNotFound      If the product with the specified ID does not
+                               exist.
+    :raises EntryNotFound:     If the tag with the specified ID does not exist.
+    :raises NothingHasChanged: If no change occurred after the update or removal.
+    """
+
+    if command not in ['add', 'remove']:
+        raise exc.UnauthorizedAccess()
+
+    data = json_body()
+    required = {'product_id': int, 'tag_id': int}
+
+    # Check all required fields
+    check_fields_and_types(data, required)
+
+    # Check if the product exists.
+    product = Product.query.filter_by(id=data['product_id']).first()
+    if not product:
+        raise exc.EntryNotFound()
+
+    # Check if the tag exists.
+    tag = Tag.query.filter_by(id=data['tag_id']).first()
+    if not tag:
+        raise exc.EntryNotFound()
+
+    if command == 'add':
+        if tag in product.tags:
+            raise exc.NothingHasChanged()
+
+        try:
+            product.tags.append(tag)
+            db.session.commit()
+        except IntegrityError:
+            raise exc.CouldNotUpdateEntry()
+
+        return jsonify({'message': 'Tag assignment has been added.'}), 201
+    else:
+        if tag not in product.tags:
+            raise exc.NothingHasChanged()
+
+        if len(product.tags) <= 1:
+            raise exc.NoRemainingTag()
+
+        try:
+            product.tags.remove(tag)
+            db.session.commit()
+        except IntegrityError:
+            raise exc.CouldNotUpdateEntry()
+
+        return jsonify({'message': 'Tag assignment has been removed.'}), 201
