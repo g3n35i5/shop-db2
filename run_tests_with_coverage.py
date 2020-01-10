a6b2661ede4c37640e7d4ba9237989630e748a6c
g3n35i5 <janfschmidt@mailbox.org>
2020-01-10 15:52:06 +0100

    Testing/Coverage: Adding a script which runs all tests with coverage
    The coverage results are then automatically displayed in the browser.

---
+++ run_tests_with_coverage.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'g3n35i5'
+
+import os
+import shutil
+import unittest
+import signal
+from http.server import HTTPServer, CGIHTTPRequestHandler
+import webbrowser
+import threading
+import time
+import sys
+import configuration as config
+
+try:
+    import coverage
+except ImportError:
+    raise ImportError("Please install the coverage module (https://pypi.python.org/pypi/coverage/)")
+
+
+class SilentHTTPHandler(CGIHTTPRequestHandler):
+    """
+    Custom HTTP handler which suppresses the console output.
+    """
+    def log_message(self, format, *args):
+        return
+
+
+def start_server(path, port=8000) -> None:
+    """
+    Start a simple webserver serving path on port
+
+    :param path: Root path for the webserver
+    :param port: Port for the webserver
+    :return:     None
+    """
+    os.chdir(path)
+    httpd = HTTPServer(("", port), SilentHTTPHandler)
+    httpd.serve_forever()
+
+
+def main() -> None:
+    # Load .coveragerc.ini configuration file
+    config_file = os.path.join(config.PATH, ".coveragerc.ini")
+    os.environ["COVERAGE_PROCESS_START"] = config_file
+    cov = coverage.coverage(config_file=config_file)
+
+    # Start coverage
+    cov.start()
+    html_cov_path = os.path.join(config.PATH, "htmlcov")
+    webserver_port = 8000
+    try:
+        tests = unittest.TestLoader().discover(os.path.join(config.PATH, "tests"))
+        unittest.TextTestRunner(verbosity=2).run(tests)
+    finally:
+        cov.stop()
+        cov.save()
+        cov.combine()
+    if os.path.exists(html_cov_path):
+        shutil.rmtree(html_cov_path)
+    cov.html_report(directory=html_cov_path)
+
+    # Start webserver
+    daemon = threading.Thread(name="Coverage Server", target=start_server, args=(html_cov_path, webserver_port))
+    daemon.setDaemon(True)
+    daemon.start()
+
+    # Open page in browser
+    webbrowser.open("http://localhost:{}".format(webserver_port))
+
+    # Keep the script alive as long it
+    print("Webserver is running on port {}. Press CTRL+C to exit".format(webserver_port))
+    while True:
+        try:
+            time.sleep(.1)
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
