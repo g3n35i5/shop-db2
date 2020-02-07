56c61ef9830f62b7cddea27f25e1873cf4048acc
g3n35i5 <janfschmidt@mailbox.org>
2020-02-07 13:34:53 +0100

    Models: Moving UserVerifications

--- shopdb/models.py
+++ shopdb/models.py
@@ -46,6 +46,23 @@ class RankUpdate(db.Model):
         return admin_id
 
 
+class UserVerification(db.Model):
+    __tablename__ = 'userverifications'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, default=func.now(), nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False,
+                        unique=True)
+
+    @validates('admin_id')
+    def validate_admin(self, key, admin_id):
+        user = User.query.filter(User.id == admin_id).first()
+        if not user or not user.is_admin:
+            raise UnauthorizedAccess()
+
+        return admin_id
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -177,23 +194,6 @@ class User(db.Model):
                   .order_by(func.sum(Purchase.amount).desc())
                   .all())
         return [purchase.product_id for purchase, _ in result]
-
-
-class UserVerification(db.Model):
-    __tablename__ = 'userverifications'
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, default=func.now(), nullable=False)
-    admin_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False,
-                        unique=True)
-
-    @validates('admin_id')
-    def validate_admin(self, key, admin_id):
-        user = User.query.filter(User.id == admin_id).first()
-        if not user or not user.is_admin:
-            raise UnauthorizedAccess()
-
-        return admin_id
 
 
 class AdminUpdate(db.Model):
